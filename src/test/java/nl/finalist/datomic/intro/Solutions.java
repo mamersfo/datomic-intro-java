380f7df17f7432efa52704fcea8450a4cb8f4dc0
Martin van Amersfoorth <mamersfo@gmail.com>
2012-10-11 23:57:38 +0200

    Several fixes after tech session
    Added Maven repo for datomic-free, Java 6 compilation, separated unit
    tests, and re-encoded data files to US-ASCII.

--- src/test/java/nl/finalist/datomic/intro/Solutions.java
+++ src/test/java/nl/finalist/datomic/intro/Solutions.java
@@ -1,33 +1,31 @@
 package nl.finalist.datomic.intro;
 
+
 public class Solutions
 {
-    // E1: Find all entities
-    String exercise1 = "[:find ?p :in $ :where [?p :name]]";
-    
-    // E2: Find all persons
-    String exercise2 = "[:find ?p :in $ :where [?p :person/height _]]";
-    
-    // E3: Find team and salary for Zlatan Ibrahimovic
-    String exercise3 = "[:find ?team ?salary ?instant :in $ ?n :where " +
-                            "[?p :name ?n] " +
-                            "[?p :player/team ?t] " + 
-                            "[?t :name ?team] " +
-                            "[?p :player/salary ?salary ?tx]]";
-    
-    // E4: List name, team and salary, ordered by salary (desc) for 2011
-    // do: db.asOf(year2011)
-    
-    // E5: Find Twitter screenName and followersCount where followersCount > a million
-    String exercise5 = "[:find ?s ?c :in $ :where [?t :twitter/screenName ?s] [?t :twitter/followersCount ?c] [(> ?c 1000000)]]";
-    
-    // E6: Find names of players who are following Robin van Persie on Twitter
-    String exercise6 = "[:find ?name :in $ ?a1 :where " +
-                            "[?p1 :name ?a1] " + 
-                            "[?p1 :player/twitter.screenName ?s1] " +
-                            "[?tw :twitter/screenName ?s1] " +
-                            "[?tw :twitter/followers ?fs] " +
-                            "[?fs :twitter/screenName ?s2] " +
-                            "[?p2 :player/twitter.screenName ?s2] " +
-                            "[?p2 :name ?name]]";
+    public static final String query1 = "[:find ?p :in $ :where " +
+                                        "[?p :name]]";
+    public static final String query2 = "[:find ?p :in $ :where " + 
+                                        "[?p :person/height]]";
+    public static final String query3 = "[:find ?t ?s :in $ ?n :where " +
+                                        "[?p :name ?n]" +
+                                        "[?p :player/salary ?s]" +
+                                        "[?p :player/team ?team]" + 
+                                        "[?team :name ?t]]";
+    public static final String query4 = "[:find ?instant :in $ ?n :where " + 
+                                        "[?p :player/salary _ ?tx]" +
+                                        "[?tx :db/txInstant ?instant]]";    
+    //  database argument should be: conn.db().asOf( year2011 )
+    public static final String query5 = "[:find ?s ?c :in $ :where " +
+                                        "[?t :twitter/screenName ?s]" +
+                                        "[?t :twitter/followersCount ?c]" +
+                                        "[(> ?c 1000000)]]";
+    public static final String query6 = "[:find ?name :in $ ?a1 :where " +
+                                        "[?p1 :name ?a1]" + 
+                                        "[?p1 :player/twitter.screenName ?s1]" +
+                                        "[?tw :twitter/screenName ?s1]" +
+                                        "[?tw :twitter/followers ?fs]" +
+                                        "[?fs :twitter/screenName ?s2]" +
+                                        "[?p2 :player/twitter.screenName ?s2]" +
+                                        "[?p2 :name ?name]]";
 }
--- src/test/java/nl/finalist/datomic/intro/Tests.java
+++ src/test/java/nl/finalist/datomic/intro/Tests.java
@@ -1,6 +1,7 @@
 package nl.finalist.datomic.intro;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Collection;
 import java.util.Date;
@@ -11,116 +12,155 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import datomic.Connection;
-import datomic.Entity;
 import datomic.Peer;
 
 public class Tests
 {
     private static final Logger LOGGER = LoggerFactory.getLogger( Tests.class );
     
-    @Test
-    public void parseDatomicFileAndRunTransaction()
-    {
-        final String uri = "datomic:mem://players";
+    private final String uri = "datomic:mem://players";
         
+    @Test
+    public void findAllEntitiesAndPersons()
+    {
+        LOGGER.info( "Exercise 1: find all entities" );
+
         LOGGER.info( "Creating and connecting to database at {}", uri );
         Connection conn = Main.createAndConnect( uri );
-        
-        // Adding schema and data (1)
-        
         LOGGER.info( "Adding schema and data with attrs: name, country, person/born, person/height, player/position" );
         Main.parseDatomicFileAndRunTransaction( "data/schema-1.dtm", conn );
         Main.parseDatomicFileAndRunTransaction( "data/data-1.dtm", conn );
         
-        // Exercise 1
-
-        LOGGER.info( "Find all entities" );
-        // Task: define the query
-        String query = "[:find ?p :in $ :where [?p :name]]";
-        Collection<List<Object>> results = Peer.q( query, conn.db() );
-        assertEquals( 153, results.size() );
-        List<Entity> entities = Helper.entities( results, conn.db() );
-        Helper.printEntities( entities );
-        
-        // Exercise 2
-        
-        LOGGER.info( "Find all persons" );
-        // Task: define the query
-        query = "[:find ?p :in $ :where [?p :person/height _]]";
-        results = Peer.q( query, conn.db() );
-        assertEquals( 85, results.size() );
+        // Task: define query1
+        String query1 = Solutions.query1;
+        assertTrue( query1 != null && query1.length() > 0 );
+        Collection<List<Object>> results = Peer.q( query1, conn.db() );
         Helper.printEntities( Helper.entities( results, conn.db() ) );
+        assertEquals( 153, results.size() );
 
-        // Adding schema and data (2)
+        // Task: define query2
+        String query2 = Solutions.query2;
+        assertTrue( query2 != null && query1.length() > 0 );
+        results = Peer.q( query2, conn.db() );
+        Helper.printEntities( Helper.entities( results, conn.db() ) );
+        assertEquals( 85, results.size() );
+    }
+    
+    @Test
+    public void findTeamAndSalaryForZlatan()
+    {
+        LOGGER.info( "Exercise 3: find team name and salary for Zlatan" );
         
+        LOGGER.info( "Creating and connecting to database at {}", uri );
+        Connection conn = Main.createAndConnect( uri );
+        LOGGER.info( "Adding schema and data with attrs: name, country, person/born, person/height, player/position" );
+        Main.parseDatomicFileAndRunTransaction( "data/schema-1.dtm", conn );
+        Main.parseDatomicFileAndRunTransaction( "data/data-1.dtm", conn );
         LOGGER.info( "Adding attributes to schema: player/team, player/salary + data" );
         Main.parseDatomicFileAndRunTransaction( "data/schema-2.dtm", conn );
         Main.loadPlayerTeamAndSalary( "data/data-2-2011.csv", conn );
-
-        // Exercise 3
         
-        LOGGER.info( "Find team and salary for Zlatan Ibrahimovic" );
         // Task: define the query
-        query = "";
-        results = Peer.q( query, conn.db(), "Zlatan Ibrahimovic" );
+        String query3 = Solutions.query3;
+        assertTrue( query3 != null && query3.length() > 0 );
+        Collection<List<Object>> results = Peer.q( query3, conn.db(), "Zlatan Ibrahimovic" );
         List<Object> tuple = results.iterator().next();        
         assertEquals( 2, tuple.size() );
         assertEquals( "AC Milan", tuple.get( 0 ) );
         assertEquals( 9.0, tuple.get( 1 ) );
-        LOGGER.info( "Added data for Zlatan Ibrahimovic, team: {}, salary: {}", tuple.toArray() );
+        LOGGER.info( "Added data for Zlatan Ibrahimovic, team: {}, salary: {} million per annum", tuple.toArray() );
+    }
+    
+    @Test
+    public void findTopEarnersForSubsequentYears()
+    {
+        LOGGER.info( "Exercise 4: find top earners for subsequent years" );
         
-        // Find instant for Zlatans salary addition
-
-        LOGGER.info( "Find instant when Zlatans salary was recorded" );
-        query = "[:find ?instant :in $ ?n :where [?p :name ?n] [?tx :db/txInstant ?instant]]";
-        results = Peer.q( query, conn.db(), "Zlatan Ibrahimovic" );
+        LOGGER.info( "Creating and connecting to database at {}", uri );
+        Connection conn = Main.createAndConnect( uri );
+        LOGGER.info( "Adding schema and data with attrs: name, country, person/born, person/height, player/position" );
+        Main.parseDatomicFileAndRunTransaction( "data/schema-1.dtm", conn );
+        Main.parseDatomicFileAndRunTransaction( "data/data-1.dtm", conn );
+        LOGGER.info( "Adding attributes to schema: player/team, player/salary + data" );
+        Main.parseDatomicFileAndRunTransaction( "data/schema-2.dtm", conn );
+        Main.loadPlayerTeamAndSalary( "data/data-2-2011.csv", conn );
+        
+        LOGGER.info( "Find instant when salaries were first recorded" );
+        // Task: define the query
+        String query4 = Solutions.query4;
+        assertTrue( query4 != null && query4.length() > 0 );
+        Collection<List<Object>> results = Peer.q( query4, conn.db(), "Zlatan Ibrahimovic" );
         Date year2011 = (Date)results.iterator().next().get( 0 );
-        LOGGER.info( "Salary data for Zlatan added on {}", year2011 );
-        
-        // Add data for 2012
-        
+        LOGGER.info( "Salary data added on {}", year2011 );
+
+        // Pause in order to discriminate between transactions
+        try { Thread.sleep( 1000 ); } catch( InterruptedException e ) {}
         LOGGER.info( "Loading player team and salary data for 2012" );
         Main.loadPlayerTeamAndSalary( "data/data-2-2012.csv", conn );
 
-        // Exercise 4
-        
-        LOGGER.info( "List name, team and salary, ordered by salary (desc) for 2011" );
-        query = "";
-        // Task: change the argument
+        LOGGER.info( "List name and salary, ordered by salary as of now (2012)" );
+        String query = "[:find ?name ?salary :in $ :where [?player :name ?name][?player :player/salary ?salary]]";
         results = Peer.q( query, conn.db() );
         List<List<Object>> values = Helper.sort( Helper.list( results ), 1, "DESC" ); 
+        assertEquals( "Samuel Eto'o", values.get( 0 ).get( 0 ) );
         Helper.printValues( values );
-        assertEquals( "Cristiano Ronaldo", values.get( 0 ).get( 0 ) );
         
-        // Add schema and data (3)
-
+        LOGGER.info( "List name and salary, ordered by salary as of last year (2011)" );
+        // Task: change database argument in order to get the facts for last year
+        results = Peer.q( query, conn.db().asOf( year2011 ) );
+        values = Helper.sort( Helper.list( results ), 1, "DESC" ); 
+        assertEquals( "Cristiano Ronaldo", values.get( 0 ).get( 0 ) );
+        Helper.printValues( values );
+    }
+    
+    @Test
+    public void findTwitterersWithOverOneMillionFollowers()
+    {
+        LOGGER.info( "Exercise 5: find a Twitter user's screenName and followersCount " +
+                     "where followersCount is over one million followers" );
+        
+        LOGGER.info( "Creating and connecting to database at {}", uri );
+        Connection conn = Main.createAndConnect( uri );
+        LOGGER.info( "Adding schema and data with attrs: name, country, person/born, person/height, player/position" );
+        Main.parseDatomicFileAndRunTransaction( "data/schema-1.dtm", conn );
+        Main.parseDatomicFileAndRunTransaction( "data/data-1.dtm", conn );
         LOGGER.info( "Adding Twitter user attributes to schema + data" );
         Main.parseDatomicFileAndRunTransaction( "data/schema-3.dtm", conn );
         Main.parseDatomicFileAndRunTransaction( "data/data-3.dtm", conn );
-
-        // Exercise 5
-
-        LOGGER.info( "Find Twitter screenName and followersCount where followersCount > a million" );
-        // Task: define the query
-        query = "";
-        results = Peer.q( query, conn.db() );
-        assertEquals( 21, results.size() );
-        values = Helper.sort( Helper.list( results ), 1, "DESC" ); 
-        Helper.printValues( values );
-
-        // Add schema and data (4)
         
+        // Task: define the query
+        String query5 = Solutions.query5;
+        assertTrue( query5 != null && query5.length() > 0 );
+        Collection<List<Object>> results = Peer.q( query5, conn.db() );
+        assertEquals( 21, results.size() );
+        List<List<Object>> values = Helper.sort( Helper.list( results ), 1, "DESC" ); 
+        Helper.printValues( values );
+    }
+    
+    @Test
+    public void findNamesOfPlayersWhoAreFollowingRobinVanPersieOnTwitter()
+    {        
+        LOGGER.info( "Exercise 6: find names of players who are following Robin van Persie on Twitter" );
+        
+        LOGGER.info( "Creating and connecting to database at {}", uri );
+        Connection conn = Main.createAndConnect( uri );
+        LOGGER.info( "Adding schema and data with attrs: name, country, person/born, person/height, player/position" );
+        Main.parseDatomicFileAndRunTransaction( "data/schema-1.dtm", conn );
+        Main.parseDatomicFileAndRunTransaction( "data/data-1.dtm", conn );
+        LOGGER.info( "Adding Twitter user attributes to schema + data" );
+        Main.parseDatomicFileAndRunTransaction( "data/schema-3.dtm", conn );
+        Main.parseDatomicFileAndRunTransaction( "data/data-3.dtm", conn );
         LOGGER.info( "Adding attributes to schema: player/twitter.screenName + data" );
         Main.parseDatomicFileAndRunTransaction( "data/schema-4.dtm", conn );
         Main.loadPlayerTwitterScreenName( "data/data-4.csv", conn );
 
-        // Exercise 6
-        
-        LOGGER.info( "Find names of players who are following Robin van Persie on Twitter" );
         // Task: define the query
-        query = "";
-        results = Peer.q( query, conn.db(), "Robin van Persie" );
+        String query6 = Solutions.query6;
+        assertTrue( query6 != null && query6.length() > 0 );
+        Collection<List<Object>> results = Peer.q( query6, conn.db(), "Robin van Persie" );
         assertEquals( 23, results.size() );
-        Helper.printValues( Helper.list( results ) );
+        List<List<Object>> values = Helper.sort( Helper.list( results ), 0, "ASC" );
+        assertEquals( "Andrei Arshavin", values.get( 0 ).get( 0 ) );
+        Helper.printValues( values );
     }    
 }
